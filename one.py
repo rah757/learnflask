3f684fc662426ba6ae8175a9613fbef2db9f7b05
rah <arrehman757@gmail.com>
2023-04-15 07:30:15 +0530

    first iteration

--- one.py
+++ one.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/") #route it to default page (homepage) by giving just /
 def home(): #homepage displaying
-    return "Hello! This is the main page <h1> Hello <h1>" 
+    return "Hello! This is the main page <h1> Hello there! <h1>" 
 
 if __name__ == "__main__":
     app.run()
